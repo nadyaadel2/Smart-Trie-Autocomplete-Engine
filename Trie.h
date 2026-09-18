46137e95ae0850b42f8a5327f3cf2a24363ffef4
nadyaadel2 <naallythy07@gmail.com>
2026-09-18 15:19:02 +0300

    Add files via upload

---
+++ Trie.h
@@ -0,0 +1,67 @@
+#ifndef TRIE_H
+#define TRIE_H
+
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+
+class TrieNode {
+public:
+    unordered_map<char, TrieNode*> children;
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() {
+        root = new TrieNode();
+    }
+
+
+    void insert(const string& word) {
+        TrieNode* current = root;
+        for (char ch : word) {
+            if (current->children.find(ch) == current->children.end()) {
+                current->children[ch] = new TrieNode();
+            }
+            current = current->children[ch];
+        }
+        current->isEndOfWord = true;
+    }
+
+
+    bool search(const string& word) {
+        TrieNode* current = root;
+        for (char ch : word) {
+            if (current->children.find(ch) == current->children.end()) {
+                return false;
+            }
+            current = current->children[ch];
+        }
+        return current->isEndOfWord;
+    }
+
+
+    bool startsWith(const string& prefix) {
+        TrieNode* current = root;
+        for (char ch : prefix) {
+            if (current->children.find(ch) == current->children.end()) {
+                return false;
+            }
+            current = current->children[ch];
+        }
+        return true;
+    }
+};
+
+#endif
